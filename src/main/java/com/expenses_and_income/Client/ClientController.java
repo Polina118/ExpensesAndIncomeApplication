4c7fd6c9cc57cb1a6a4b55544efeb2209236c1d3
Polina118 <p_guk@inbox.ru>
2022-06-08 16:17:58 +0300

    added clientcontroller and clientservice

---
+++ src/main/java/com/expenses_and_income/Client/ClientController.java
@@ -0,0 +1,2 @@
+package com.expenses_and_income.Client;public class ClientController {
+}
--- src/main/java/com/expenses_and_income/ExpensesAndIncomeApplication.java
+++ src/main/java/com/expenses_and_income/ExpensesAndIncomeApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExpensesAndIncomeApplication {
-
+public class {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesAndIncomeApplication.class, args);
 	}
